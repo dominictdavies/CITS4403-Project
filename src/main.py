68206011be3e12195ce1c5dd1c6d983552781394
Dominic Davies <dominictdavies@gmail.com>
2025-10-07 16:38:08 +0800

    Make main run the simulation

--- src/main.py
+++ src/main.py
@@ -1,25 +1,5 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-import mesa
-from mesa.discrete_space import CellAgent, OrthogonalMooreGrid
-from mesa.visualization import SolaraViz, SpaceRenderer, make_plot_component
-from mesa.visualization.components import AgentPortrayalStyle
+from src.visualisation import run_simulation
 
 
-def agent_portrayal(agent):
-    return AgentPortrayalStyle(color="tab:orange", size=50)
-
-
-model_params = {
-    "n": {
-        "type": "SliderInt",
-        "value": 50,
-        "label": "Number of agents:",
-        "min": 10,
-        "max": 100,
-        "step": 1,
-    },
-    "width": 10,
-    "height": 10,
-}
+if __name__ == "__main__":
+    run_simulation()
