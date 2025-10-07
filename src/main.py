cc1962ddd8058dc58d2596c16e8cb07e1a84195d
Dominic Davies <dominictdavies@gmail.com>
2025-10-07 17:57:22 +0800

    Set more defaults

--- src/main.py
+++ src/main.py
@@ -6,14 +6,14 @@ from src.visualisation import run_simulation
 
 if __name__ == "__main__":
     model = InfectionModel(
-        N=100,
-        width=50,
-        height=50,
+        N=128,
+        width=1280,
+        height=1280,
         speed=1.0,
-        collision_radius=0.1,
-        contact_radius=2.0,
+        collision_radius=0.1,  # TODO: Fix collision_radius
+        contact_radius=10.0,
         infection_prob=0.5,
-        initial_infected=2,
+        initial_infected=1,
         seed=42,
     )
 
