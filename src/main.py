ab0af3c23eeef451c57894bb100641adc1626628
Dominic Davies <dominictdavies@gmail.com>
2025-10-13 21:26:02 +0800

    Expose population parameters

--- src/main.py
+++ src/main.py
@@ -2,9 +2,17 @@
 
 from src.model import InfectionModel
 from src.visualisation import run_simulation
-from utils.config import SimulationConfig
+from utils.config import PopulationConfig, SimulationConfig
 
 if __name__ == "__main__":
-    config = SimulationConfig()
+    population = PopulationConfig(
+        num_people=128,
+        initial_infected=1,
+        vaccinated_rate=0.2,
+        vaccinated_effect=1.0,
+        recovered_effect=1.0,
+    )
+
+    config = SimulationConfig(population=population)
     model = InfectionModel(config=config)
     run_simulation(model)
